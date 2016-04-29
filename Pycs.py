de58891837332fee97bdea2183e1b1c9f5d93a5d
IIIIIHIIIII <nikhil@tuta.io>
2016-04-29 11:07:39 +0530

    Added TradeEngineHistoricData methods

--- Pycs.py
+++ Pycs.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 class CSError(Exception):
-	pass
+    pass
 
 
 class Pycs:
@@ -12,18 +12,17 @@ class Pycs:
         self.wid = wid
 
     def process(self,url,data):
-        
-		response = requests.post("https://api.coinsecure.in/v0%s" %(url), 
-                                    data=json.dumps(data), 
-                                    headers={"content-type":"text/json"}).json()
+        response = requests.post("https://api.coinsecure.in/v0%s" %(url), 
+                            data=json.dumps(data), 
+                            headers={"content-type":"text/json"}).json()
 
-		if "error" in response.values() or "error" in response.keys(): 
-			message = response["error"] if("error" in response.keys()) else response["message"]
-			raise CSError(message)
+        if "error" in response.values() or "error" in response.keys(): 
+            message = response["error"] if("error" in response.keys()) else response["message"]
+            raise CSError(message)
 
-		return response
+        return response
 
-       
+
     def getallaccountsconfirmedbalance(self):
         """
                 Gets the total confirmed balance across all accounts and addresses
@@ -66,198 +65,303 @@ class Pycs:
 
     def getallaccountsunconfirmedbalance(self):
         """
-    	Gets the total unconfirmed balance across all accounts and addresses
-		"""
+        Gets the total unconfirmed balance across all accounts and addresses
+        """
 
-    	data = {
-        	"apiKey" : self.key
-    	}
+        data = {
+                        "apiKey" : self.key
+                }
 
-    	url = "/auth/getallaccountsunconfirmedbalance"
+        url = "/auth/getallaccountsunconfirmedbalance"
 
         return self.process(url,data)
 
     def getaccountaddresses(self):
         """
-    	Gets the Bitcoin address based on account/ wallet name.
-		"""
+        Gets the Bitcoin address based on account/ wallet name.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"walletID" : self.wid
-    	}
+        data = {
+                        "apiKey" : self.key,
+                        "walletID" : self.wid
+                }
 
-    	url = "/auth/getaccountaddresses"
+        url = "/auth/getaccountaddresses"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
     def getaddressunconfirmedbalance(self,address):
         """
-    	Gets the total unconfirmed balance on a user addresses.
-		"""
+        Gets the total unconfirmed balance on a user addresses.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-        	"address" : address
-    	}
+        data = {
+                        "apiKey" : self.key,
+            "address" : address
+        }
 
-    	url = "/auth/getaddressunconfirmedbalance"
+        url = "/auth/getaddressunconfirmedbalance"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
     def getaccountunconfirmedbalance(self):
         """
-   		Gets the unconfirmed balance on an account/ wallet.
-		"""
+        Gets the unconfirmed balance on an account/ wallet.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-        	"walletID" : self.wid
-    	}
+        data = {
+                                "apiKey" : self.key,
+                                "walletID" : self.wid
+                        }
 
-    	url = "/auth/getaccountunconfirmedbalance"
+        url = "/auth/getaccountunconfirmedbalance"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
     def getaddressconfirmedbalance(self,address):
         """
-   		Gets the total confirmed balance on a user addresses
-		"""
+        Gets the total confirmed balance on a user addresses
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-        	"address" : address
-    	}
+        data = {
+            "apiKey" : self.key,
+            "address" : address
+        }
 
-    	url = "/auth/getaddressconfirmedbalance"
+        url = "/auth/getaddressconfirmedbalance"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
     def getunverifiedaccountwithdraws(self):
         """
-		Gets a list of unverified Withdrawals for a Wallet.
-		"""
+        Gets a list of unverified Withdrawals for a Wallet.
+        """
 
-    	data = {
-        	"apiKey" : self.key
-    	}
+        data = {
+                        "apiKey" : self.key
+        }
 
-    	url = "/auth/getunverifiedaccountwithdraws"
+        url = "/auth/getunverifiedaccountwithdraws"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
     def newaccount(self,walletname,info):
         """
-		Create a new Account/ Wallet.
-		"""
+        Create a new Account/ Wallet.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"walletName" : walletname,
-			"info" : info
-    	}
+        data = {
+            "apiKey" : self.key,
+            "walletName" : walletname,
+            "info" : info
+        }
 
-    	url = "/auth/newaccount"
+        url = "/auth/newaccount"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def sendtoaddress(self,fromwid,satoshis,msg,toaddress):
         """
-		Sends Bitcoin to any On Chain Address. Fees of 0.0001 BTC is added to withdrawal amount.
-		"""
+        Sends Bitcoin to any On Chain Address. Fees of 0.0001 BTC is added to withdrawal amount.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"fromWalletID" : fromwid,
-			"satoshis" : satoshis,
-			"msg" : msg,
-			"toAddress" : toaddress
-    	}
+        data = {
+            "apiKey" : self.key,
+            "fromWalletID" : fromwid,
+            "satoshis" : satoshis,
+            "msg" : msg,
+            "toAddress" : toaddress
+            }
 
-    	url = "/auth/sendtoaddress"
+        url = "/auth/sendtoaddress"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def verifyaccountwithdrawcode(self,code):
         """
-		Verifies a Withdrawal Code and processes the Transaction.
-		"""
+        Verifies a Withdrawal Code and processes the Transaction.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"code" : code
-    	}
+        data = {
+            "apiKey" : self.key,
+            "code" : code
+        }
 
-    	url = "/auth/verifyaccountwithdrawcode"
+        url = "/auth/verifyaccountwithdrawcode"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def cancelunverifiedaccountcoinwithdrawal(self,address,satoshis,withdrawid,reason):
         """
-		Cancels an unverified Withdrawal Code the Transaction.
-		"""
+        Cancels an unverified Withdrawal Code the Transaction.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"address" : address,
-			"satoshis" : satoshis,
-			"withdrawID" : withdrawid,
-			"reason" : reason
-    	}
+        data = {
+            "apiKey" : self.key,
+            "address" : address,
+            "satoshis" : satoshis,
+            "withdrawID" : withdrawid,
+            "reason" : reason
+            }
 
-    	url = "/auth/cancelunverifiedaccountcoinwithdrawal"
+        url = "/auth/cancelunverifiedaccountcoinwithdrawal"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def getaccountwithdrawverifycode(self,withdrawid):
         """
-		Gets the account withdrawal Verification Code for a Withdraw ID.
-		"""
+        Gets the account withdrawal Verification Code for a Withdraw ID.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"withdrawID" : withdrawid
-    	}
+        data = {
+            "apiKey" : self.key,
+            "withdrawID" : withdrawid
+        }
 
-    	url = "/auth/getaccountwithdrawverifycode"
+        url = "/auth/getaccountwithdrawverifycode"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def sendtotradeengine(self,fromwalletid,satoshis,msg):
         """
-		Sends Bitcoin to Trade Engine. Can send as low as 1 satoshi.
-		"""
+        Sends Bitcoin to Trade Engine. Can send as low as 1 satoshi.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"fromWalletID" : fromwalletid,
-			"satoshis" : satoshis,
-			"msg" : msg
-    	}
+        data = {
+            "apiKey" : self.key,
+            "fromWalletID" : fromwalletid,
+            "satoshis" : satoshis,
+            "msg" : msg
+        }
 
-    	url = "/auth/sendtotradeengine"
+        url = "/auth/sendtotradeengine"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
 
     def newaddress(self,walletid,info):
         """
-		Generates a new Bitcoin Address in an account/ wallet.
-		"""
+        Generates a new Bitcoin Address in an account/ wallet.
+        """
 
-    	data = {
-        	"apiKey" : self.key,
-			"walletID" : walletid,
-			"info" : info
-    	}
+        data = {
+            "apiKey" : self.key,
+            "walletID" : walletid,
+            "info" : info
+        }
 
-    	url = "/auth/newaddress"
+        url = "/auth/newaddress"
 
-    	return self.process(url,data)
+        return self.process(url,data)
 
-	
+
+    def getmaxuserbid(self):
+        """
+        Returns highest bid details from users orders.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/getmaxuserbid"
+        return self.process(url,data)
+
+
+    def cancelleduserasks(self):
+        """
+        Returns all cancelled Ask Orders in Json. The Rate is displayed paisa
+        and Volume in Satoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/cancelleduserasks"
+        return self.process(url,data)
+
+    def completeduserasks(self):
+        """
+        Returns all completed ask Orders in Json. The Rate is displayed in
+        Paisa and Volume in Satoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/completeduserasks"
+        return self.process(url,data)
+
+
+    def cancelleduserbids(self):
+        """
+        Returns all cancelled Bid Orders in Json. The Rate is displayed in
+        Paisa and Volume in SAtoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/cancelleduserbids"
+        return self.process(url,data)
+
+    def alluserasks(self):
+        """
+        Returns all User Asks Orders in Json. The Rate is displayed in Paisa
+        and Volume in Satoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+        
+        url = "/auth/alluserasks"
+        return self.process(url,data)
+
+    def completeduserbids(self):
+        """
+        Returns all completed Bid Orders in Json. The RAte is displayed in
+        Paisa and Volume in Satoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/completeduserbids"
+        return self.process(url,data)
+
+    
+    def alluserbids(self):
+        """
+        Returns all User Bid Orders in Json. The Rate is displayed in 
+        Paisa and Volume in Satoshis.
+        """
+
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/alluserbids"
+        return self.process(url,data)
+
+
+    def getminuserask(self):
+        """
+        Returns lowest ask details from users orders.
+        """
+    
+        data = {
+            "apiKey" : self.key
+        }
+
+        url = "/auth/getminuserask"
+        return self.process(url,data)
